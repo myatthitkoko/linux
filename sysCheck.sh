eb8565b26cbf4f8026c67116aa5913e68bd7ed2b
Myat Thit Ko Ko <MyatThitKoKo@outlook.fr>
2026-05-12 11:20:58 +0000

    cron for sysCheck.sh log files every five mins

--- sysCheck.sh
+++ sysCheck.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
+#logged every 5 minutes | edit at crontab -e
+
+OUT="/workspaces/linux/systemLogs/systemReport_$(TZ="America/Los_Angeles" date +"%F_%H-%M-%S").log"
+mkdir -p /workspaces/linux/systemLogs
+
+{
 
 echo "=============================================="
 echo "              SYSTEM REPORT"
 echo "=============================================="
 
-echo "CPU Usage:"
 top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: User:", $2, "% System:", $4, "% Idle:", $8 "%"}'
 
 echo
 echo "Memory Usage:"
-free -h
+free -h | head -2
 
 DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
+echo
 echo "Disk Usage: ${DISK_USAGE}%"
 
 if [ "$DISK_USAGE" -gt 80 ]; then
-  echo "⚠ WARNING: Disk usage is high!"
+  echo "WARNING: Disk usage is high!"
+else
+  echo "Disk usage is ok"
 fi
 
 MEM_AVAILABLE=$(free | awk '/Mem:/ {printf("%.0f"), $7/$2 * 100.0}')
 
 echo "Memory Available: ${MEM_AVAILABLE}%"
 
-
 echo
 
-echo "Generated at: $(date)"
+curl -s --head https://8.8.8.8 >/dev/null && echo "Internet: CONNECTED" || echo "Internet: DISCONNECTED"
+
+echo "Generated at: $(TZ="America/Los_Angeles" date)" 
+
+} > "$OUT"
+
+cat "$OUT"

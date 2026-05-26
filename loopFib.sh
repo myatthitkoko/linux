560481105ed05fee6de35f4bc21603a392bff1ed
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-05-26 00:05:38 -0700

    Update suffix logic for Fibonacci term output
    Modify suffix determination for Fibonacci sequence output to handle exceptions for 11, 12, and 13.

--- loopFib.sh
+++ loopFib.sh
@@ -9,12 +9,16 @@ b=1
 
 for ((i = 0; i <= num; i++))
 do
-    case $((i % 10)) in
-        1) suffix="st" ;;
-        2) suffix="nd" ;;
-        3) suffix="rd" ;;
-        *) suffix="th" ;;
-    esac
+    if [[ i -ne 11 ]] && [[ i -ne 12 ]] && [[ i -ne 13 ]]; then
+        case $((i % 10)) in
+            1) suffix="st" ;;
+            2) suffix="nd" ;;
+            3) suffix="rd" ;;
+            *) suffix="th" ;;
+        esac
+    else
+        suffix="th";
+    fi
 
     echo "${i}${suffix} term = $a"
 
